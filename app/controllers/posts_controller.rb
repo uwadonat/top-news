a6a03ee6ae32c4583f7eba7ca8f6115b681f0f56
uwadonat <uwadonat@gmail.com>
2021-03-31 05:02:37 -0400

    add edit method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,19 @@ class PostsController < ApplicationController
       
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to post_path
+        else
+            render :edit
+        end
+    end
+
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"
-  resources :users 
+  
   resources :posts
-
+  resources :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
